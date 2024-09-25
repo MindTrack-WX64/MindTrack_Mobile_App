efec821e32c8d895ed9e072e23a9bd49a41b9aaf
Miguel Angel Ybañez Esquerre <arcangel080902@gmail.com>
2024-09-24 20:50:39 -0500

    Update AppDatabase.kt

--- app/src/main/java/com/example/mindtrack_mobile_app/repository/AppDatabase.kt
+++ app/src/main/java/com/example/mindtrack_mobile_app/repository/AppDatabase.kt
@@ -1,4 +1,28 @@
 package com.example.mindtrack_mobile_app.repository
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.mindtrack_mobile_app.models.User
+
+
+@Database(entities = arrayOf(User::class), version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun getDao():UserDao
+
+    companion object{
+        private var INSTANCE: AppDatabase?= null
+
+        fun getInstance(context: Context): AppDatabase{
+            if(INSTANCE ==null){
+                INSTANCE = Room
+                    .databaseBuilder(context, AppDatabase::class.java, "MindTrack.db")
+                    .allowMainThreadQueries()
+                    .build()
+            }
+            return INSTANCE as AppDatabase
+        }
+    }
 }
