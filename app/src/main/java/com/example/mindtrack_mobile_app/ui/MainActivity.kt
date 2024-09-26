90dc736e5f8b26c25e45fc135b796c0ea0375b78
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-26 10:59:43 -0500

    refactor: adding logic to login button

--- app/src/main/java/com/example/mindtrack_mobile_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/mindtrack_mobile_app/ui/MainActivity.kt
@@ -1,6 +1,8 @@
-package com.example.mindtrack_mobile_app.UI
+package com.example.mindtrack_mobile_app.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +18,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainBoardActivity::class.java)
+            startActivity(intent)
         }
     }
 }
