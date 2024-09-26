8e6ba84f363440d041a98d61d36a2121ac33e120
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-26 11:01:08 -0500

    feat: adding design to activities

---
+++ app/src/main/java/com/example/mindtrack_mobile_app/ui/MainBoardActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mindtrack_mobile_app.ui
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mindtrack_mobile_app.R
+import com.example.mindtrack_mobile_app.adapters.PatientAdapter
+import com.example.mindtrack_mobile_app.models.Patient
+
+class MainBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_board)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Example data
+        val patients = listOf(
+            Patient(1, 1, "John Doe", "123456789","12344@hotmail.com",25),
+            Patient(2, 1, "Jane Smith", "987654321","123456@hotmail.com",25)
+        )
+
+        val adapter = PatientAdapter(patients)
+        recyclerView.adapter = adapter
+    }
+}
