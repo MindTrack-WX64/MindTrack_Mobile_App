2fa0892510a9b14420a3e3fe6862ed171484c310
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-26 11:16:17 -0500

    feat: adding Add Patient Activity

---
+++ app/src/main/java/com/example/mindtrack_mobile_app/ui/AddPatientActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mindtrack_mobile_app.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mindtrack_mobile_app.R
+
+class AddPatientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_patient)
+    }
+}
