f7069adfeed9f357168cd7ab58d7ddae9df0e9b4
Miguel Angel Ybañez Esquerre <arcangel080902@gmail.com>
2024-09-26 10:39:33 -0500

    layoutaddAppointment

--- app/src/main/java/com/example/mindtrack_mobile_app/UI/CalendarAppointmentActivity.kt
+++ app/src/main/java/com/example/mindtrack_mobile_app/UI/CalendarAppointmentActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mindtrack_mobile_app.UI
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +16,8 @@ import com.example.mindtrack_mobile_app.models.AppointmentProvider
 
 class CalendarAppointmentActivity : AppCompatActivity() {
 
+    private lateinit var btnNewDate: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,8 +27,22 @@ class CalendarAppointmentActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initComponents()
+        initListeners()
         initRecyclerView()
     }
+
+    private fun initComponents() {
+        btnNewDate = findViewById(R.id.btnNewDate)
+    }
+    private fun initListeners() {
+        btnNewDate.setOnClickListener {
+            val intent = Intent(this, AddAppointmentActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 
     private fun initRecyclerView(){
         val recyclerView = findViewById<RecyclerView>(R.id.rvAppointment)
